a8cccd9e467659ddc709e92b6e10008283faae99
travisbrigman <travis1000@gmail.com>
2020-11-02 15:52:21 -0600

    chapter 7 working

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,23 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
+import { GameProvider } from "./game/GameProvider"
+import { GameList } from "./game/GameList";
 
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
-        </main>
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          backgroundColor: "lightgoldenrodyellow",
+        }}
+      >
+        <GameProvider>
+          <Route exact path="/">
+            <GameList />
+          </Route>
+        </GameProvider>
+      </main>
     </>
-}
+  );
+};
